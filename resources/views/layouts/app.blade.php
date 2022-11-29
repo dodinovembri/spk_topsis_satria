bac24ad70fb08d31285ca906256d594dcdefaaa9
dodinovembri <dodinovembri32gmail.com>
2022-11-29 10:49:38 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-    <title>Destinasi Wisata</title>
+    <title>Studio Foto Prewedding</title>
     <meta name="HandheldFriendly" content="True" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
