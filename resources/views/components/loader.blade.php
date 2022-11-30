c366a0e748643e28f8d66460f21646d4a01aadba
dodinovembri <dodinovembri32gmail.com>
2022-11-30 13:06:13 +0700

    update

--- resources/views/components/loader.blade.php
+++ resources/views/components/loader.blade.php
@@ -1,6 +1,6 @@
 <div id="loader-overlay">
     <div class="loader">
-        <img src="{{ asset('logo/logo.jpg') }}" width="100px" alt="Pathway blog" />
+        <img src="{{ asset('logo/logo.jpg') }}" width="100px" alt="Studio Foto Prewedding" />
         <i class="fas fa-spinner fa-pulse"></i>
     </div>
 </div>
--- resources/views/destination/index.blade.php
+++ resources/views/destination/index.blade.php
@@ -21,7 +21,7 @@
 
 <div id="loader-overlay">
     <div class="loader">
-        <img src="{{ asset('assets/content/images/2021/04/logo.jpg') }}" alt="Pathway blog" />
+        <img src="{{ asset('logo/logo.jpg') }}" width="100px" alt="Studio Foto Prewedding" />
         <i class="fas fa-spinner fa-pulse"></i>
     </div>
 </div>
@@ -32,22 +32,16 @@
 
     <div class="frontpage-slider-posts style-one">
         <?php if (isset($alternative->video)) { ?>
-            
-            <div id="container" style="height: 85%;"></div>
 
-            <script>
-                var div = document.getElementById('container');
-                var PSV = new PhotoSphereViewer({
-                    panorama: "{{ asset('img/alternative') }}/{{ $alternative->video }}",
-                    container: div,
-                    time_anim: 3000,
-                    navbar: true,
-                    navbar_style: {
-                        backgroundColor: 'rgba(58, 67, 77, 0.7)'
-                    },
-                });
-            </script>
-        <?php } else{ ?>
+
+            <div style="margin: 20px;">
+                <center>
+                    <video controls>
+                        <source src="{{ asset('img/alternative') }}/{{ $alternative->video }}" type="video/mp4">
+                    </video>
+                </center>
+            </div>
+        <?php } else { ?>
             <p>Video tidak tersedia</p>
         <?php } ?>
     </div>
